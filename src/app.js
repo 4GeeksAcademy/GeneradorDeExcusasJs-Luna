b9a3abf43e3bc2b9c8ed52ef84eda95149573f7f
LunaB28 <lunab2815@gmail.com>
2025-03-31 18:35:52 +0000

    Final Commit

--- src/app.js
+++ src/app.js
@@ -2,10 +2,25 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+function RandomArray(array) {
+  return Math.floor(Math.random() * array.length);
+}
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let arraywho = ["El perro", "Mi abuela", "El cartero", "Mi pájaro"];
+  let arrayaction = ["se comió", "pisó", "aplastó", "rompió"];
+  let arraywhat = ["mi tarea", "mi teléfono", "el auto"];
+  let who = RandomArray(arraywho);
+  let action = RandomArray(arrayaction);
+  let what = RandomArray(arraywhat);
+  let when = RandomArray(arraywhen);
+  let excuse = document.getElementById("excuse");
+  excuse.innerText =
+    arraywho[who] +
+    " " +
+    arrayaction[action] +
+    " " +
+    arraywhat[what] +
+    " " +
+    arraywhen[when];
 };
