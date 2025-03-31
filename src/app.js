5b2e6e6b849c9878a47659d3ffce1de77a5ae565
LunaB28 <lunab2815@gmail.com>
2025-03-31 18:41:52 +0000

    cambie el titulo de la pagina

--- src/app.js
+++ src/app.js
@@ -10,6 +10,13 @@ window.onload = function() {
   let arraywho = ["El perro", "Mi abuela", "El cartero", "Mi pájaro"];
   let arrayaction = ["se comió", "pisó", "aplastó", "rompió"];
   let arraywhat = ["mi tarea", "mi teléfono", "el auto"];
+  let arraywhen = [
+    "antes de la clase",
+    "cuando dormía",
+    "mientras hacía ejercicio",
+    "durante mi almuerzo",
+    "mientras oraba"
+  ];
   let who = RandomArray(arraywho);
   let action = RandomArray(arrayaction);
   let what = RandomArray(arraywhat);
