5450089ed4355af8c00b57ae76115dc1757cdb0c
LunaB28 <lunab2815@gmail.com>
2025-04-04 19:04:35 +0000

    CamelCast y separacion llamadas a funcion y array app.js: Apliqué Camel cast al nombre de los array e hice un espacio entre estos y los calls, así como el log.

--- src/app.js
+++ src/app.js
@@ -7,27 +7,17 @@ function RandomArray(array) {
 }
 
 window.onload = function() {
-  let arraywho = ["El perro", "Mi abuela", "El cartero", "Mi pájaro"];
-  let arrayaction = ["se comió", "pisó", "aplastó", "rompió"];
-  let arraywhat = ["mi tarea", "mi teléfono", "el auto"];
-  let arraywhen = [
-    "antes de la clase",
-    "cuando dormía",
-    "mientras hacía ejercicio",
-    "durante mi almuerzo",
-    "mientras oraba"
-  ];
-  let who = RandomArray(arraywho);
-  let action = RandomArray(arrayaction);
-  let what = RandomArray(arraywhat);
-  let when = RandomArray(arraywhen);
+  let arrayWho = ["El perro", "Mi abuela", "El cartero", "Mi pájaro"];
+  let arrayAction = ["se comió", "pisó", "aplastó", "rompió"];
+  let arrayWhat = ["mi tarea", "mi teléfono", "el auto"];
+  let arrayWhen = ["antes de la clase","cuando dormía","mientras hacía ejercicio","durante mi almuerzo","mientras oraba"];
+ 
+  let who = RandomArray(arrayWho);
+  let action = RandomArray(arrayAction);
+  let what = RandomArray(arrayWhat);
+  let when = RandomArray(arrayWhen);
+
   let excuse = document.getElementById("excuse");
-  excuse.innerText =
-    arraywho[who] +
-    " " +
-    arrayaction[action] +
-    " " +
-    arraywhat[what] +
-    " " +
-    arraywhen[when];
+
+  excuse.innerText = arrayWho[who] + " " + arrayAction[action] + " " + arrayWhat[what] + " " + arrayWhen[when];
 };
